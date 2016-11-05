69c15d7dbc39fae689a27570892d09e2a43a9eaf
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-05 11:34:46 -0700

    Added Blog Post model with properties

--- MvcAuth/Models/BlogPost.cs
+++ MvcAuth/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,16 @@ namespace MvcAuth.Models
 {
     public class BlogPost
     {
+        [Key]
+        public int BlogPostId { get; set; }
 
+        [MinLength(1)]
+        [MaxLength(300)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        public DateTime PublishedAt { get; set; }
+
+        // But what about the Author?!?!?!
     }
 }

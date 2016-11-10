d38b45ba39f9dce294a001480b3938b28c5c225d
VikingPaul <wip321@yahoo.com>
2016-11-09 21:27:13 -0600

    end of the first day.

---
+++ Tweeter/DAL/TwerpContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Tweeter.Models;
+
+namespace Tweeter.DAL
+{
+    public class TwerpContext : ApplicationDbContext
+    {
+        public virtual DbSet<Twerp> Twerps { get; set; }
+        public virtual DbSet<Twit> Twits { get; set; }
+    }
+}
---
+++ Tweeter/Models/Twerp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tweeter.Models
+{
+    public class Twerp
+    {
+        public int TwerpId { get; set; }
+        public string Message { get; set; }
+        public Twit Author { get; set; }
+        public string ImgUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<Twerp> Replies { get; set; }
+    }
+}

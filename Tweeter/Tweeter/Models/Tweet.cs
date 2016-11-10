5bdb18056d2478e0ed7598197e7171a946dbaa1d
complikatyed <katye.russell@gmail.com>
2016-11-09 20:57:52 -0600

    Add DAL with TweeterContext and new tweet model

---
+++ Tweeter/Tweeter/Models/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tweeter.Models
+{
+    public class Tweet
+    {
+        public int TweetId { get; set; }
+        public string Message { get; set; }
+        public int Author { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<Tweet> Replies { get; set; }  // Self referential
+          
+    }
+}

3939eda7cbdd929b9d298393170057961963a006
Sandy Jones <sandynashv@gmail.com>
2016-11-10 12:52:00 -0600

    Username View

--- Tweeter/Models/AccountViewModels.cs
+++ Tweeter/Models/AccountViewModels.cs
@@ -72,6 +72,12 @@ namespace Tweeter.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 

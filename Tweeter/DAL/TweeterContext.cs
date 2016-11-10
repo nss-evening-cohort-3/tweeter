f47e9d387cf6ed44dddc2a773f360dc8d6c8477e
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-09 18:56:10 -0800

    Initial project with Simple Tweet Model and Tweeter Context

---
+++ Tweeter/DAL/TweeterContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Tweeter.Models;
+
+namespace Tweeter.DAL
+{
+    public class TweeterContext : ApplicationDbContext
+    {
+        public virtual DbSet<Tweet> Tweets { get; set; }
+    }
+}

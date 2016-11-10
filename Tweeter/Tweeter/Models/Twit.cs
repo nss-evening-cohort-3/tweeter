13e62653b659e21b46114957d09faaa10475d821
complikatyed <katye.russell@gmail.com>
2016-11-09 21:16:03 -0600

    Adding applicationUser twit model

---
+++ Tweeter/Tweeter/Models/Twit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tweeter.Models
+{
+    public class Twit
+    {
+        public int TwitId { get; set; }
+        public ApplicationUser BaseUser { get; set; }
+        public List<Twit> Follows { get; set; }
+    }
+}

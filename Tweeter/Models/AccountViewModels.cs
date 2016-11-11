fe260968d6df263dbc8a3208c133af3d23ac74be
Bernard Anderson <bernardanderson@gmail.com>
2016-11-11 08:53:48 -0600

    Added UserName and Code for UserName A username field was added to the Register.cshtml file and the subsequent code added to the AccountViewModels.cs

--- Tweeter/Models/AccountViewModels.cs
+++ Tweeter/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace Tweeter.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }

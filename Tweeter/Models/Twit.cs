6eb1b0dab96f7a640c8b913aa9cb15733dc92419
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-12 11:57:52 -0800

    modifying the Twit profile model requies a migration.

--- Tweeter/Models/Twit.cs
+++ Tweeter/Models/Twit.cs
@@ -12,6 +12,10 @@ namespace Tweeter.Models
         public List<Twit> Follows { get; set; }
         /*
          * Maybe just add a Username field here
+         * Then you'll need to mock the Twit table to return List<Twit>
+         * This clearly means that you need to EITHER: 
+         *     1. Re-scaffold the InitialCreate migration OR 
+         *     2. Create a new Migration that adds this field.S
          */
     }
 }

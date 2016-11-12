ec78932c8a3add327aa9cf133929a400fe6a4081
Scott Schaffer <scottpschaffer@hotmail.com>
2016-11-11 16:33:19 -0800

    I have no idea how this is all working

--- Tweeter/Models/AccountViewModels.cs
+++ Tweeter/Models/AccountViewModels.cs
@@ -64,6 +64,12 @@ namespace Tweeter.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]

e268c7ab21a7042b0e2b5b088d9c1846ad55485b
Sandy Jones <sandynashv@gmail.com>
2016-11-12 20:29:19 -0600

    repo work and repo tests

--- Tweeter/DAL/TweeterRepository.cs
+++ Tweeter/DAL/TweeterRepository.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Tweeter.Models;
 
 namespace Tweeter.DAL
 {
     public class TweeterRepository
     {
         public TweeterContext Context { get; set; }
+        public TweeterRepository()
+        {
+            Context = new TweeterContext();
+        }
         public TweeterRepository(TweeterContext _context)
         {
             Context = _context;
         }
-
-        public TweeterRepository() {}
-
+        public List<Twit> GetTwits()
+        {
+            int j = 1;
+            return Context.Twits.ToList();
+        }
+        public void AddTwit(Twit _twit)
+        {
+            Context.Twits.Add(_twit);
+            Context.SaveChanges();
+        }
 
     }
 }

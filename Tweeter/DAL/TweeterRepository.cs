45ae28be14e6cae37b47a9c83372d7e7bf4f9587
Sandy Jones <sandynashv@gmail.com>
2016-11-14 12:35:44 -0600

    working on GetTwits function

--- Tweeter/DAL/TweeterRepository.cs
+++ Tweeter/DAL/TweeterRepository.cs
@@ -19,14 +19,43 @@ namespace Tweeter.DAL
         }
         public List<Twit> GetTwits()
         {
+            List<Twit> TwitList = new List<Twit>(); 
             int j = 1;
-            return Context.Twits.ToList();
+            //create an actual list of twits
+            //add each Twit to a list called TwitList
+            TwitList.Add.Twit();
+            return TwitList;
+            //tried: 
+            //return List<Twit>(Twit).ToString();
+            //return List<Twit>.ToString();
+            //return List<Twit>().ToString();
+            //return List<Twits>().ToString();
+            //instantiated TwitList and then returned:
+            //TwitList.Add(Twit);
+            //TwitList.Add.Twit;
+            //TwitList.Add.Twit();
+            //
+            //
         }
         public void AddTwit(Twit _twit)
         {
-            Context.Twits.Add(_twit);
+            Context.Twit.Add(_twit);
             Context.SaveChanges();
         }
+        public Twit findTwitByTwitId(object Twit)
+        {
+            Twit foundTwit = new Models.Twit();
 
+            if (foundTwit != null)
+            {
+                return foundTwit;
+            }
+            else
+            {
+                return null;
+            }
+
+            
+        }
     }
 }

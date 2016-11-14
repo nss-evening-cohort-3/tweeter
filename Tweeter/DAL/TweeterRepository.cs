e26649c6dfd00f56b4a141cc0855801398e5fb26
Odigene Joseph <Odigene12@gmail.com>
2016-11-14 12:23:24 -0600

    Stuck on Getting Usernames. Tests seems to be setup right, but getting an empty list that doesn't contain test data.

--- Tweeter/DAL/TweeterRepository.cs
+++ Tweeter/DAL/TweeterRepository.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
+using Tweeter.Models;
 
 namespace Tweeter.DAL
 {
     public class TweeterRepository
     {
+        
+
         public TweeterContext Context { get; set; }
+        public UserManager<ApplicationUser> UserManagerContext { get; set; }
         public TweeterRepository(TweeterContext _context)
         {
             Context = _context;
         }
 
-        public TweeterRepository() {}
+        public TweeterRepository()
+        {
 
+        }
 
+        public List<string> GetUsernames()
+        {
+            return Context.Users.Select(u => u.UserName).ToList();
+        }
     }
 }

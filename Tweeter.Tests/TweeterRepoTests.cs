c4a9377479b44b2250e6c95987f60001ad990fc4
Chris Barrett <c.barrett@me.com>
2016-11-14 16:02:03 -0800

    added tests and mock contexts

---
+++ Tweeter.Tests/TweeterRepoTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tweeter.DAL;
+using Moq;
+using Tweeter.Models;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tweeter.Tests
+{
+    [TestClass]
+    public class TweeterRepoTests
+    {
+
+        private Mock<TweeterContext> MockContext;
+        private Mock<DbSet<Twit>> TweeterUsers;
+        private List<Twit> twits;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            MockContext = new Mock<TweeterContext>();
+            TweeterUsers = new Mock<DbSet<Twit>>();
+
+
+            MockContext.Setup(x => x.TweeterUsers).Returns(TweeterUsers.Object);
+
+            twits = new List<Twit>
+            {
+                new Twit
+                {
+                   Username = "user-1"
+                },
+                new Twit
+                {
+                   Username = "user-2"
+                }
+            };
+
+            ConnectMocksToDatastore();
+        }
+
+        public void TestCleanup()
+        {
+            MockContext = null;
+            TweeterUsers = null;
+            twits = null;
+        }
+
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryableTwits = twits.AsQueryable();
+            TweeterUsers.As<IQueryable<Twit>>().Setup(x => x.Provider).Returns(queryableTwits.Provider);
+            TweeterUsers.As<IQueryable<Twit>>().Setup(x => x.Expression).Returns(queryableTwits.Expression);
+            TweeterUsers.As<IQueryable<Twit>>().Setup(x => x.ElementType).Returns(queryableTwits.ElementType);
+            TweeterUsers.As<IQueryable<Twit>>().Setup(x => x.GetEnumerator()).Returns(queryableTwits.GetEnumerator());
+        }
+
+
+
+        [TestMethod]
+        public void CanMakeInstanceOfRepo()
+        {
+            TweeterRepository myRepo = new TweeterRepository();
+            Assert.IsNotNull(myRepo);
+        }
+
+        [TestMethod]
+        public void EnsureMyRepoCanAccessContext()
+        {
+            TweeterRepository myRepo = new TweeterRepository(MockContext.Object);
+        }
+
+        [TestMethod]
+        public void CanGetUsernames()
+        {
+            TweeterRepository myRepo = new TweeterRepository(MockContext.Object);
+            List<Twit> myTwits = myRepo.GetUsernames();
+            CollectionAssert.AreEqual(myTwits, twits);
+        }
+
+        [TestMethod]
+        public void DoesUsernameExist()
+        {
+            TweeterRepository myRepo = new TweeterRepository(MockContext.Object);
+            bool hasUsername = myRepo.UsernameExists("user-1");
+            Assert.IsTrue(hasUsername);
+        }
+    }
+}
---
+++ Tweeter/DAL/TweeterRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tweeter.Models;
+
+namespace Tweeter.DAL
+{
+    public class TweeterRepository
+    {
+        public TweeterContext Context { get; set; }
+
+        public TweeterRepository(TweeterContext context)
+        {
+            Context = context;
+        }
+
+        public TweeterRepository() { }
+
+        // Get all Usernames
+        public List<Twit> GetUsernames()
+        {
+            return Context.TweeterUsers.ToList();
+        }
+
+        // Check if username exists
+        public bool UsernameExists(string username)
+        {
+            return GetUsernames().Any(m => m.Username == username);
+        }
+    }
+}

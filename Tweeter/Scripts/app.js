a5d6c156b67a5294a80f09b38b5cc92debdb32c3
Sandy Jones <sandynashv@gmail.com>
2016-11-15 08:45:42 -0600

    added app

---
+++ Tweeter/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿$("#register-username").focusout(function () {
+    // alert("defocused!!!"); this is correct so far
+    console.log($(this).val());
+   // $(this).val();//this refers to #register-username; this refers to current element you are sitting at
+    $.ajax({
+        url: "/api/TwitUsername?candidate=" + $(this).val(),
+
+        method: 'GET'
+    }).success(function (response) {
+        console.log(response);
+        if(response.exists){
+            $("username-ans").addClass("glyphicon-ok");
+        } else {
+            $("username-ans").addClass("glyphicon-remove");
+        }
+    }).fail(function (error) {
+        console.log(error);
+    });
+
+});
+$("#register-username").focusin(function () {
+    $("#username-ans").removeClass("glyphicon-ok");
+
+};

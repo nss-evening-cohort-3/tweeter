6251f62fd7e9523e3ba04080a3e1bbcb379d37b5
Tim Maddux <timothymaddux@gmail.com>
2016-11-15 15:37:29 -0600

    Form submit blocked when respnse.exists

--- Tweeter/Scripts/app.js
+++ Tweeter/Scripts/app.js
@@ -1,8 +1,8 @@
 ﻿$("#register-username").keyup(function () {
 
     // first step is removing a UI reponse to the AJAX call by taking out classes
-    $("#username-ans").removeClass("glyphicon-ok glyphicon-remove btn-success btn-danger");
-
+    $("#username-ans").removeClass("glyphicon-ok glyphicon-remove");
+    $("form").submit(true);
     // get value from username field input
     var input = $("#register-username").val();
     $.ajax({
@@ -12,13 +12,15 @@
         //console.log(response.exists);
         //added stuff so that field can't be empty
         if (!response.exists && input !== "") {
+            $("form").submit(true);
             $("#register-submit").prop("disabled", false);
             $("#username-ans").addClass("glyphicon-ok");
             //$("#register-submit").addClass("btn-success");
         } else {
+            $("form").submit(false);
             $("#register-submit").prop("disabled", true);
             $("#username-ans").addClass("glyphicon-remove");
-            //$("#register-submit").addClass("btn-danger");
+            //$("#register-submit").addClass("btn-danger");  //not great practice. slips up a lot. 
         }
     }).fail(function (error) {
         console.log(error);

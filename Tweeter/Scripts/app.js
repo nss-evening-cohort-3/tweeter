e0ca29626a5a21a167e045f8344bbd36d1beffc0
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-14 19:06:41 -0800

    Hit API endpoint to check for available username,

---
+++ Tweeter/Scripts/app.js
@@ -0,0 +1,23 @@
+﻿$("#register-username").focusout(function () {
+    //alert("defocused!!!");
+    //console.log($(this).val());
+    //$("#username-ans").addClass("hidden");
+    $.ajax({
+        url: "/api/TwitUsername?candidate=" + $(this).val(),
+        method: 'GET'
+    }).success(function (response) {
+        console.log(response);
+        if (response.exists) {
+            $("#username-ans").addClass("glyphicon-ok");
+        } else {
+            $("#username-ans").addClass("glyphicon-remove");
+        }
+    }).fail(function (error) {
+        console.log(error);
+    });
+});
+
+$("#register-username").focusin(function () {
+    $("#username-ans").removeClass("glyphicon-ok");
+    $("#username-ans").removeClass("glyphicon-remove");
+});

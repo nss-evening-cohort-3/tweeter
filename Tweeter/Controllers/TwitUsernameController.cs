6f357e956356166c7187987a17928fb972cf9c8a
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-14 18:51:25 -0600

    updated api to hit repo specifc username check

---
+++ Tweeter/Controllers/TwitUsernameController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tweeter.DAL;
+
+namespace Tweeter.Controllers
+{
+    public class TwitUsernameController : ApiController
+    {
+        TweeterRepo repo = new TweeterRepo();
+
+        // GET: api/TwitUsername
+        public IEnumerable<string> Get()
+        {
+            return repo.GetAllUsernames();
+        }
+
+        // GET: api/TwitUsername/5
+        public bool Get(string username)
+        {
+            return repo.UsernameExists(username);
+        }
+
+        // POST: api/TwitUsername
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TwitUsername/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TwitUsername/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

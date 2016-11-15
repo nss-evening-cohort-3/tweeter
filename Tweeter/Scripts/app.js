25fab07bdf1fd832186f955cabf740875ff3bbc0
Tim Maddux <timothymaddux@gmail.com>
2016-11-15 16:22:40 -0600

    UI improvements.

--- Tweeter/Scripts/app.js
+++ Tweeter/Scripts/app.js
@@ -2,6 +2,7 @@
 
     // first step is removing a UI reponse to the AJAX call by taking out classes
     $("#username-ans").removeClass("glyphicon-ok glyphicon-remove");
+    $("#submit-msg-area").html("");
     $("form").submit(true);
     // get value from username field input
     var input = $("#register-username").val();
@@ -20,6 +21,7 @@
             $("form").submit(false);
             $("#register-submit").prop("disabled", true);
             $("#username-ans").addClass("glyphicon-remove");
+            $("#submit-msg-area").html("<div id='submit-msg' style='margin-left:-89px; margin-top:8px'>Try again dude.</div>");
             //$("#register-submit").addClass("btn-danger");  //not great practice. slips up a lot. 
         }
     }).fail(function (error) {

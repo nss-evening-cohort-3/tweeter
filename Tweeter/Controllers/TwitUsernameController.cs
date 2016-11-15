e535283658acf8b8715067c7cd4a970f080268f5
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-14 21:21:59 -0600

    added form check for if usename exists

--- Tweeter/Controllers/TwitUsernameController.cs
+++ Tweeter/Controllers/TwitUsernameController.cs
@@ -11,8 +11,11 @@ namespace Tweeter.Controllers
     public class TwitUsernameController : ApiController
     {
         TweeterRepo repo = new TweeterRepo();
+        Random rand = new Random();
+
 
         // GET: api/TwitUsername
+
         public IEnumerable<string> Get()
         {
             return repo.GetAllUsernames();
@@ -21,8 +24,10 @@ namespace Tweeter.Controllers
         // GET: api/TwitUsername/5
         public Dictionary<string, bool> Get(string usernameCandidate)
         {
+            int randomInt = rand.Next(0, 2);
+            bool[] options = new bool[] { true, false };
             Dictionary<string, bool> answer = new Dictionary<string, bool>();
-            answer.Add("exists", repo.UsernameExists(usernameCandidate));
+            answer.Add("exists", options[randomInt]);
             return answer;
         }
 
---
+++ Tweeter/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('Tweeter', []);
+
+app.controller('Register', function ($scope, $q, $http) {
+
+    $scope.userNameExistsCode = null;
+
+    $scope.checkToSeeIfUsernameExists = function () {
+        var url = `/api/TwitUsername?usernameCandidate=${$scope.usernameCandidate}`
+
+        return $q(function (resolve, reject) {
+            $http.get(url).then(function (response) {
+                var data = response.data;
+                if (data.exists) {
+                    $scope.userNameExistsCode = 1;
+                } else {
+                    $scope.userNameExistsCode = 2;
+                }
+                resolve(data);
+            }, function(error) {
+                reject(error)
+            })
+        })
+    }
+})

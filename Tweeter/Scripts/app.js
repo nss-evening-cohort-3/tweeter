6288d640f92347d62cb8e59a4abb89fb61b54fae
David Cramb <david.cramb@gmail.com>
2016-11-15 21:57:00 -0600

    Disable register button if username exists in database. glyphicon for appropriate response displays. Glyphicon is still living on an island in the middle of the damn page.

--- Tweeter/Scripts/app.js
+++ Tweeter/Scripts/app.js
@@ -11,8 +11,13 @@ userNameField.focusout(function () {
         console.log(response);
         if (response.exists) {
             $('#username-ans').addClass("glyphicon-remove");
+            $('#username-ans').removeClass("glyphicon-ok");
+            $('input[value="Register"]').addClass("disabled").removeClass("btn-info");
+
         } else {
             $('#username-ans').addClass("glyphicon-ok");
+            $('#username-ans').removeClass("glyphicon-remove ");
+            $('input[value="Register"]').removeClass("disabled").addClass("btn-info");
         }
     }).fail(function (error) {
         console.log('Failed!' + error)

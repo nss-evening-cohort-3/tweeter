41da96319fbfd22991366114967c0ca1e51e1f76
MATT BRUTON <the.bruton@gmail.com>
2016-11-17 11:49:34 -0600

    added my version of current registration work to project

--- Tweeter/Scripts/app.js
+++ Tweeter/Scripts/app.js
@@ -1,49 +1,45 @@
-﻿$("#register-username").keyup(function () {
-    //$("form").submit(true);
-    $("#username-ans").removeClass("glyphicon-ok");
-    $("#username-ans").removeClass("glyphicon-remove");
-    $.ajax({
-        url: "/api/TwitUsername?candidate=" + $(this).val(),
-        method: 'GET'
-    }).success(function (response) {
-        console.log(response.exists);
-        if (!response.exists) {
-            $("#submit").attr("disabled", "disabled");
-            $("#username-ans").addClass("glyphicon-remove");
-            
-        } else {
-            $("#submit").removeAttr("disabled");
-            $("#username-ans").addClass("glyphicon-ok");
-        }
-    }).fail(function (error) {
-        console.log(error);
-    });
-});
+﻿let showUsernameInputValidStyling = () => {
+    $('#username-help-block').addClass('hidden');
+    $('.form-group__input--username').addClass('has-success');
+};
+let showUsernameInputErrorStyling = () => {
+    $('#username-help-block').removeClass('hidden');
+    $('.form-group__input--username').addClass('has-error');
+};
 
+let disableRegisterSubmitButton = () => {
+    $('.button__registration--submit').addClass('disabled btn-danger');
+    $('.button__registration--submit').attr('disabled', 'disabled');
+};
 
-
-/*
-$("#register-username").focusout(function () {
-    //alert("defocused!!!");
-    //console.log($(this).val());
-    //$("#username-ans").addClass("hidden");
-    $.ajax({
-        url: "/api/TwitUsername?candidate=" + $(this).val(),
-        method: 'GET'
-    }).success(function (response) {
-        console.log(response);
-        if (response.exists) {
-            $("#username-ans").addClass("glyphicon-ok");
-        } else {
-            $("#username-ans").addClass("glyphicon-remove");
-        }
-    }).fail(function (error) {
-        console.log(error);
-    });
-});
+let enableRegisterSubmitButton = () => {
+    $('.button__registration--submit').removeClass('disabled btn-danger');
+    $('.button__registration--submit').removeAttr('disabled');
+};
 
 $("#register-username").focusin(function () {
-    $("#username-ans").removeClass("glyphicon-ok");
-    $("#username-ans").removeClass("glyphicon-remove");
+    $('#username-help-block').show();
 });
-*/
+
+$("#register-username").focusout(function () {
+    $('#username-help-block').hide();
+});
+
+$("#register-username")
+    .keyup(function () {
+        $('.form-group__input--username').removeClass('has-success has-error');
+        $.ajax({
+            url: `/api/TwitUsername?candidate=${$(this).val()}`,
+            method: 'GET'
+        }).success(function (response) {
+            if (!response.exists) {
+                showUsernameInputValidStyling();
+                enableRegisterSubmitButton();
+            } else {
+                showUsernameInputErrorStyling();
+                disableRegisterSubmitButton();
+            }
+        }).fail(function (error) {
+            console.log(error);
+        });
+    })

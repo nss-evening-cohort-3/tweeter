bb277b39a6888823f68193aec914327803792c7d
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-16 17:00:54 -0800

    Unit tested AddTweet and RemoveTweet methods.

--- Tweeter/DAL/TweeterRepository.cs
+++ Tweeter/DAL/TweeterRepository.cs
@@ -46,5 +46,38 @@ namespace Tweeter.DAL
             return false;
             
         }
+
+        public void AddTweet(Tweet a_tweet)
+        {
+            Context.Tweets.Add(a_tweet);
+            Context.SaveChanges();
+        }
+
+        public void AddTweet(string username, string tweet_message)
+        {
+            Twit found_twit = Context.TweeterUsers.FirstOrDefault(u => u.BaseUser.UserName == username);
+            if (found_twit != null)
+            {
+                Tweet new_tweet = new Tweet
+                {
+                    Message = tweet_message,
+                    CreatedAt = DateTime.Now,
+                    Author = found_twit
+                };
+                Context.Tweets.Add(new_tweet);
+                Context.SaveChanges();
+            }
+        }
+
+        public Tweet RemoveTweet(int tweet_id)
+        {
+            Tweet found_tweet = Context.Tweets.FirstOrDefault(t => t.TweetId == tweet_id);
+            if (found_tweet != null)
+            {
+                Context.Tweets.Remove(found_tweet);
+                Context.SaveChanges();
+            }
+            return found_tweet;
+        }
     }
 }

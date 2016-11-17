481a765f6ab68a71ae33ed9bb2a0cb16684465fd
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-16 19:34:06 -0800

    Added TweetViewModel for easy Post submissions

---
+++ Tweeter/Models/TweeterViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tweeter.Models
+{
+    public class TweeterViewModels
+    {
+        public class TweetViewModel
+        {
+            [Required]
+            [MinLength(2)]
+            public string Message { get; set; }
+
+            public string ImageURL { get; set; }
+
+        }
+    }
+}

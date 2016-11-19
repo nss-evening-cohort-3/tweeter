c2dc3eacf4d5e8da3f8f79194fcd52beba3e9844
David Cramb <david.cramb@gmail.com>
2016-11-19 09:30:10 -0600

    Join Herobar on main page now only displays if user is not logged in. Is hidden upon user authentication

--- Tweeter/Controllers/HomeController.cs
+++ Tweeter/Controllers/HomeController.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Tweeter.Models;
 
 namespace Tweeter.Controllers
 {
     public class HomeController : Controller
     {
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+        bool isAuthed;
+        
+
         public ActionResult Index()
         {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            if(ModelState.IsValid)
+            {
+                ViewBag.IsAuthed = true;
+            }
+            else
+            {
+                ViewBag.IsAuthed = false;
+            }
             return View();
         }
 

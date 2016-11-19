0e6e45af42c57e6d74b8b5b59e9483401cadd913
MATT BRUTON <the.bruton@gmail.com>
2016-11-19 10:51:59 -0600

    added angular, making call to api to retrieve tweets

---
+++ Tweeter/Scripts/ng-app.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("tweetController", function ($scope, $http) {
+
+    $scope.tweetsToDisplay = [];
+
+    $scope.getAllTweets = function () {
+        $scope.tweetsToDisplay.length = 0;
+        $http({
+            method: 'GET',
+            url: '/api/Tweet/'
+        }).then(function successCallback(response) {
+            for (var i = 0; i < response.data.length; i++){
+                $scope.tweetsToDisplay.push(response.data[i]);
+                console.log($scope.tweetsToDisplay);
+            };
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+    }
+
+
+});

ff9a7894f4698b8696549b4f1049e76b5ebe2591
Ryan B <admirallasers@gmail.com>
2016-11-19 05:14:38 -0600

    moved username field, added join button

--- Tweeter/Models/AccountViewModels.cs
+++ Tweeter/Models/AccountViewModels.cs
@@ -70,6 +70,7 @@ namespace Tweeter.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(15, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [Display(Name = "Username")]
         public string Username { get; set; }
 

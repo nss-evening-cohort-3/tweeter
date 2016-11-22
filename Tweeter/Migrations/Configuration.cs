a28183e0e00cb9f28665c9f9d874aad6b4fcd431
Bernard Anderson <bernardanderson@gmail.com>
2016-11-21 20:24:54 -0600

    Seeded Twit-Bot

--- Tweeter/Migrations/Configuration.cs
+++ Tweeter/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Tweeter.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,6 +15,15 @@ namespace Tweeter.Migrations
 
         protected override void Seed(Tweeter.DAL.TweeterContext context)
         {
+            // May not need this fake twit_bot at all...
+            //Twit twit_bot = new Twit { BaseUser = new ApplicationUser { UserName = "TwitBot" } };
+            Twit twit_bot = new Twit { TwitId = 2 };
+
+            context.TweeterUsers.AddOrUpdate(
+                p => p.TwitId,
+                twit_bot
+                );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 

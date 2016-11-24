7a6386dae5915d9ce44f9277ad54cc17bf993f06
Bernard Anderson <bernard.anderson@gmail.com>
2016-11-24 12:56:21 -0600

    Added Delete Btn Functions to delete a tweet.

--- Tweeter/Scripts/Tweeter-page.js
+++ Tweeter/Scripts/Tweeter-page.js
@@ -1,29 +1,42 @@
 ﻿$(
     $.get("api/Tweet", function (data) {
-        console.log(data);
-        if (data.length > 0) {
-
-            let tweetBoxes = "";
-            for (var i = 0; i < data.length; i++) {
-                
-                tweetBoxes += `<div class="row text-center">`;
-                tweetBoxes += `  <div class="col-xs-6 col-sm-offset-3">`;
-                tweetBoxes += `    <img class="tweeterimg" src="${data[i].ImageURL}">`;
-                tweetBoxes += `    <p>Message: ${data[i].Message}</p>`;
-                tweetBoxes += `    <div class="btn-group" role="group" aria-label="...">`;
-                tweetBoxes += `      <button type="button" class="btn btn-default">Retweet</button>`;
-                tweetBoxes += `      <button type="button" class="btn btn-default">Delete</button>`;
-                tweetBoxes += `    </div>`;
-                tweetBoxes += `  </div>`;
-                tweetBoxes += `</div>`;
-            }
-            $("div.tweetbox").html(tweetBoxes);
-        } else {
-            $("div.tweetbox").html("<p>No Tweets today!</p>");
-        }
+        displayTweets(data);
     })
 );
 
+var displayTweets = function(data) {
+    if (data.length > 0) {
+        let tweetBoxes = "";
+        for (var i = 0; i < data.length; i++)
+        {
+            tweetBoxes += `<div id="${data[i].TweetId}" class="row text-center mainTweetDiv">`;
+            tweetBoxes += `  <div class="col-xs-6 col-sm-offset-3">`;
+            tweetBoxes += `    <img class="tweeterimg" src="${data[i].ImageURL}">`;
+            tweetBoxes += `    <p>Message: ${data[i].Message}</p>`;
+            tweetBoxes += `    <div class="btn-group" role="group" aria-label="...">`;
+            tweetBoxes += `      <button type="button" class="btn btn-default">Retweet</button>`;
+            tweetBoxes += `      <button type="button" class="btn btn-default delete">Delete</button>`;
+            tweetBoxes += `    </div>`;
+            tweetBoxes += `  </div>`;
+            tweetBoxes += `</div>`;
+        }
+        $("div.tweetbox").html(tweetBoxes);
+    } else {
+    $("div.tweetbox").html("<p>No Tweets today!</p>");
+    }
+};
+
+$('.tweetbox').click(function(event) {
+    if ( $(event.target).hasClass("delete")) {
+        let deleteTweetId = $(event.target).parents(".mainTweetDiv").attr("id");
+        $.ajax({
+            type: "DELETE",
+            url: `api/Tweet/${deleteTweetId}`,
+            success: console.log("Delete Successful")
+        });
+    }
+});
+
 $("#submit-tweet").click(function() {
-    $.post("/api/Tweet", { message: "Here is a message", ImageURL: "Blank" });
+    console.log($('input.composer').val());
 });
